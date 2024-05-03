d67cfab3c423d96bcb877b005ea1e9ed5aab73e6
Brashokish <130168063+Brashokish@users.noreply.github.com>
2024-05-03 21:39:39 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,150 @@
+
+const express = require("express");
+const app = express();
+
+
+
+
+
+const pino = require("pino");
+let { toBuffer } = require("qrcode");
+const path = require('path');
+const fs = require("fs-extra");
+const { Boom } = require("@hapi/boom");
+const PORT = process.env.PORT ||  5000
+const MESSAGE = process.env.MESSAGE ||  `
+*🚨 CONGRATULATIONS 🎊*
+
+*QR HAS BEEN SCANNED SUCCESSFULY* ✅
+
+*Gɪᴠᴇ ᴀ ꜱᴛᴀʀ ᴛᴏ ʀᴇᴘᴏ ꜰᴏʀ ᴄᴏᴜʀᴀɢᴇ* 🌀
+║❒https://github.com/Brashokish/Legacy-Md 
+
+ _Join *LEGACY-MD* Channel for Updates_
+║❒https://whatsapp.com/channel/0029VaUJjME4Y9lf2giQJC3t
+
+*JOIN OUR WhatsApp Gʀᴏᴜᴘ*
+║❒https://chat.whatsapp.com/ETxO5WC60vO36yvTQ4eFMh
+
+*FOLLOW THE DEVELOPER* 🪄 
+║❒ https://instagram.com/Brashokish 
+
+
+       *𝐋𝐄𝐆𝐀𝐂𝐘-𝐌𝐃*
+`
+
+
+
+
+
+
+
+
+
+
+
+if (fs.existsSync('./auth_info_baileys')) {
+    fs.emptyDirSync(__dirname + '/auth_info_baileys');
+  };
+  
+  app.use("/", async(req, res) => {
+
+  const { default: GiftedWASocket, useMultiFileAuthState, Browsers, delay,DisconnectReason, makeInMemoryStore, } = require("@whiskeysockets/baileys");
+  const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) })
+  async function GIFTED() {
+    const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys')
+    try {
+      let Smd =GiftedWASocket({ 
+        printQRInTerminal: false,
+        logger: pino({ level: "silent" }), 
+        browser: Browsers.baileys("Desktop"),
+        auth: state 
+        });
+
+
+      Smd.ev.on("connection.update", async (s) => {
+        const { connection, lastDisconnect, qr } = s;
+        if (qr) { res.end(await toBuffer(qr)); }
+
+
+        if (connection == "open"){
+          await delay(3000);
+          let user = Smd.user.id;
+
+
+//===========================================================================================
+//===============================  SESSION ID    ===========================================
+//===========================================================================================
+
+          let CREDS = fs.readFileSync(__dirname + '/auth_info_baileys/creds.json')
+          var Scan_Id = Buffer.from(CREDS).toString('base64')
+         // res.json({status:true,Scan_Id })
+          console.log(`
+====================  SESSION ID  ==========================                   
+SESSION-ID ==> ${Scan_Id}
+-------------------   SESSION CLOSED   -----------------------
+`)
+
+
+          let msgsss = await Smd.sendMessage(user, { text:  Scan_Id });
+          await Smd.sendMessage(user, { text: MESSAGE } , { quoted : msgsss });
+          await delay(1000);
+          try{ await fs.emptyDirSync(__dirname+'/auth_info_baileys'); }catch(e){}
+
+
+        }
+
+        Smd.ev.on('creds.update', saveCreds)
+
+        if (connection === "close") {            
+            let reason = new Boom(lastDisconnect?.error)?.output.statusCode
+            // console.log("Reason : ",DisconnectReason[reason])
+            if (reason === DisconnectReason.connectionClosed) {
+              console.log("Connection closed!")
+             // GIFTED().catch(err => console.log(err));
+            } else if (reason === DisconnectReason.connectionLost) {
+                console.log("Connection Lost from Server!")
+            //  GIFTEDL().catch(err => console.log(err));
+            } else if (reason === DisconnectReason.restartRequired) {
+                console.log("Restart Required, Restarting...")
+              GIFTED().catch(err => console.log(err));
+            } else if (reason === DisconnectReason.timedOut) {
+                console.log("Connection TimedOut!")
+             // GIFTED().catch(err => console.log(err));
+            }  else {
+                console.log('Connection closed with bot. Please run again.');
+                console.log(reason)
+              //process.exit(0)
+            }
+          }
+
+
+
+      });
+    } catch (err) {
+        console.log(err);
+       await fs.emptyDirSync(__dirname+'/auth_info_baileys'); 
+    }
+  }
+
+
+
+
+
+
+
+
+  GIFTED().catch(async(err) => {
+    console.log(err)
+    await fs.emptyDirSync(__dirname+'/auth_info_baileys'); 
+
+
+    //// MADE WITH KISH TECH
+
+});
+
+
+  })
+
+
+app.listen(PORT, () => console.log(`App listened on port http://localhost:${PORT}`));
